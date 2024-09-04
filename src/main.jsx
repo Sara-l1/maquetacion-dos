26a5f281b0a1a8358a45afda3d5e619a0e31cf3f
Sara-l1 <loperasara00@gmail.com>
2024-09-03 20:52:13 -0500

    cambio 1

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="body">
+      <header className="header">
+        <div className="enlace">Enlace</div>
+        <div className="enlace">Enlace</div>
+      </header>
+      <main className="main">
+        <aside className="aside"></aside>
+        <section className="section">
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+          <div className="img"></div>
+        </section>
+      </main>
+      <footer className="footer"></footer>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
